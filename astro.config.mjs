6c7d1bbe6095a5295f133b370a5e318f8bc9b0fe
jefische <jefische86@gmail.com>
2026-05-08 14:51:57 -0500

    update astro config domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astrofy-template.netlify.app",
+	site: "https://jfischer.dev",
 	integrations: [mdx(), sitemap(), tailwind()],
 	markdown: {
 		remarkPlugins: [remarkMath],
